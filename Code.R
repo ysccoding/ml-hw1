793b9b17b8f90afc1ceab94fefbc1290ab381035
carlyzhao <carly.zjl@gmail.com>
2020-01-23 13:39:15 -0600

    adding comments pull out result of regression

--- Code.R
+++ Code.R
@@ -2,10 +2,13 @@
 #1.#
 
 set.seed(123)
+
+# generate training data #
 x = runif(100,-1,1)
 e = rnorm(100)
 y = 1.8*x+2+e
 
+# generate test data #
 xt = runif(10000,-1,1)
 et = rnorm(10000)
 yt = 1.8*xt+2+et
@@ -18,3 +21,4 @@ abline(2,1.8,col="red")
 #3.#
 
 fit = lm(y~x)
+summary(fit)
