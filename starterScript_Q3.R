9c0da10c26194ef3fb6332ea880743981f059a57
ysccoding <richard.has.yan2@gmail.com>
2020-01-23 14:39:39 -0600

    Adding Initial Starter Script to Parse out Amazon Data

---
+++ starterScript_Q3.R
@@ -0,0 +1,61 @@
+PackageList=c("MASS", 
+              "ISLR",
+              "animation",
+              "ElemStatLearn",
+              "glmnet",
+              "textir",
+              "nnet",
+              "methods",
+              "statmod",
+              "stats",
+              "graphics",
+              "RCurl",
+              "jsonlite",
+              "tools",
+              "utils",
+              "data.table",
+              "gbm",
+              "ggplot2",
+              "randomForest",
+              "tree",
+              "class",
+              "kknn",
+              "e1071",
+              "data.table",
+              "R.utils",
+              "recommenderlab")
+NewPackages=PackageList[!(PackageList %in% 
+                            installed.packages()[,"Package"])]
+if(length(NewPackages)) install.packages(NewPackages)
+lapply(PackageList,require,character.only=TRUE)#array function
+
+
+download.file('https://github.com/ChicagoBoothML/MLClassData/raw/master/Amazon/videoGames.json.gz', 'videoGames.json.gz')
+
+fileConnection <- gzcon(file("videoGames.json.gz", "rb"))
+InputData = stream_in(fileConnection)
+
+
+ratingData = as(InputData[c("reviewerID", "itemID", "rating")], "realRatingMatrix")
+
+# we keep users that have rated more than 2 video games
+ratingData = ratingData[rowCounts(ratingData) > 2,]
+
+# we will focus only on popular video games that have 
+# been rated by more than 3 times
+ratingData = ratingData[,colCounts(ratingData) > 3]
+
+# we are left with this many users and items
+dim(ratingData)
+
+# example on how to recommend using Popular method
+r = Recommender(ratingData, method="POPULAR")
+
+# recommend 5 items to user it row 13
+rec = predict(r, ratingData[13, ], type="topNList", n=5)
+as(rec, "list")
+
+# predict ratings 
+rec = predict(r, ratingData[13, ], type="ratings")
+as(rec, "matrix")
+
