cbdc57a5e9e8262080611815bd5dc5a3b6392667
ysccoding <richard.has.yan2@gmail.com>
2020-01-24 19:26:29 -0600

    Adding code for Question 3(completed) onto final code sheet

--- starterScript_Q3.R
+++ starterScript_Q3.R
@@ -1,3 +1,5 @@
+### Q3 Setup ####
+
 PackageList=c("MASS", 
               "ISLR",
               "animation",
@@ -59,3 +61,43 @@ as(rec, "list")
 rec = predict(r, ratingData[13, ], type="ratings")
 as(rec, "matrix")
 
+#### Q3.1 - Find User that has rated the most amount of video games ####
+userIndex = which.max(rowCounts(ratingData, na.rm = TRUE)) # Find Row(Users) with Highest non-NA ratings
+mostRatedUser = rownames(ratingData)[userIndex] 
+mostRatedUser # Printout - TODO: REMOVE
+
+#### Q3.2 - Find Video Game that has the most user ratings ####
+videoGameIndex = which.max(colCounts(ratingData, na.rm = TRUE)) # Find Col(Games) with highest non-NA ratings
+mostRatedVideoGame = colnames(ratingData)[videoGameIndex]
+mostRatedVideoGame # Printout - TODO: REMOVE
+
+#### Q3.3 - Find User that is most Similar to “U141954350” ####
+userID = "U141954350"
+
+# Transform NA's to 0s for Similarity Analysis
+transformedRatingData <- as(ratingData,"matrix") # coerce to matrix so that NA can be transformed
+transformedRatingData <- replace(transformedRatingData, is.na(transformedRatingData),0) # replace all NA with 0
+transformedRatingData <- as(transformedRatingData, "realRatingMatrix") # coerce back to realRatingMatrix
+
+# Gather Matrix of User & non-User ratings for Similarity Comparison
+userRatings = transformedRatingData[userID]
+otherUserRatings = transformedRatingData[!rownames(transformedRatingData) == userID]
+
+# Using Pearson method for Ratings Similarities
+sim <- similarity(userRatings, otherUserRatings, method = "pearson", which = "users") # Finding similarity vector of all other users
+similarUser = colnames(sim)[which.max(sim)] # Finding Most Similar User
+similarUser # Printout - TODO: REMOVE
+
+#### Q3.4 - Recommend a video game to the user “U141954350 ####
+userID = "U141954350"
+currentUser = ratingData[userID]
+
+# Train Data for Recommendation Model
+scheme = evaluationScheme(transformedRatingData, method="cross-validation", k = 15, train = 0.8, given = 2)
+trainedRatings = getData(scheme, "train")
+
+# Predict 
+recModel = Recommender(trainedRatings, method = "POPULAR")
+rec = predict(recModel, currentUser, type = "topNList", n = 5)
+rec = as(rec, "list")
+rec # Printout - TODO: REMOVE
